f48992a4d82e090135b266e42bc67a7bc8b1dc7a
Elisabeth <elisabeth_fung@mymail.sutd.edu.sg>
2024-08-06 13:51:57 +0800

    update experience section

--- portfolio/src/components/Experience/Experience.jsx
+++ portfolio/src/components/Experience/Experience.jsx
@@ -14,8 +14,9 @@ export const Experience = () => {
                 </div>
                 <div className={styles.boxItem}>
                     <ul>
-                        <li className={styles.item}>Implemented a PowerBI dashboard to display real-time updates utilizing streaming datasets</li> 
-                        <li className={styles.item}>Created a Python script to extract data from PostgreSQL, perform intermediate calculations, and send it as JSON payloads via POST requests for dynamic visualization in PowerBI.</li>
+                        <li className={styles.item}>Created a Power BI dashboard with real-time streaming to update dashboards in real time</li> 
+                        <li className={styles.item}>Created a Python script to extract data from PostgreSQL, perform intermediate calculations, and send it as JSON payloads via POST requests to the PowerBI API.</li>
+                        <li className={styles.item}>Created a PowerBI dashboard using a DirectQuery data source</li>
                         <li className={styles.item}>Researched Predictive Maintenance and Fault Detection Models </li>
                     </ul>
                 </div>
@@ -28,8 +29,8 @@ export const Experience = () => {
                 </div>
                 <div className={styles.boxItem}>
                     <ul>
-                        <li className={styles.item}>Created a technical design file to convey the requirements</li>
-                        <li className={styles.item}>Implemented code changes in the SPRING framework (SpringBatch and SpringBoot)</li>
+                        <li className={styles.item}>Created a technical design file to convey business requirements</li>
+                        <li className={styles.item}>Implemented Java code changes in the SPRING framework (SpringBatch and SpringBoot)</li>
                         <li className={styles.item}>Tested in controlled environments (SIT and UAT)</li>
                         <li className={styles.item}>Identified and fixed bugs by analyzing logs via AWS CloudWatch and querying the database using PostgreSQL</li>  
                     </ul>
